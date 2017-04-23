bf26e27f902b5bdd8f15e17dd0c6d554ebc0b995
Wei Wei <lyweiwei@outlook.com>
2017-04-23 17:40:14 +0800

    Support disabled attribute for menu item

--- js/dropdown-item.js
+++ js/dropdown-item.js
@@ -6,20 +6,25 @@ export function renderDropdownItem(dropdownItem) {
     classes: [],
     id: _.uniqueId('dropdown-item-'),
     text: '',
+    disabled: false,
     iconLeft: null,
     iconRight: null,
     tabindex: -1,
     onClick: null,
   }, dropdownItem);
+
+  if (options.disabled) {
+    options.classes = _.union(options.classes, ['disabled']);
+  }
+
   const html = dropdownItemTemplate(options);
   const events = {};
 
   const { id, onClick } = options;
 
-  if (_.isFunction(onClick)) {
+  if (_.isFunction(onClick) && !options.disabled) {
     events[`click .dropdown-item#${id}`] = onClick;
   }
 
   return { html, events };
 }
-
